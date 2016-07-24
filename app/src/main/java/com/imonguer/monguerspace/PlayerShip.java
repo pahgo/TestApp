7c55d2d15562b19f0be65a4a0b7a49caf74d4d75
spenalva <penalvapeni@gmail.com>
2016-07-25 01:32:00 +0200

    First stable version

---
+++ app/src/main/java/com/imonguer/monguerspace/PlayerShip.java
@@ -0,0 +1,102 @@
+package com.imonguer.monguerspace;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+/**
+ * Created by Usuario on 23/07/2016.
+ */
+public class PlayerShip {
+    private final int MAX_SPEED = 25;
+    private final int MIN_SPEED = 1;
+    private final int GRAVITIY = -12;
+    private int maxY;
+    private int minY;
+    private Bitmap bitmap;
+    private int x;
+    private int y;
+    private int speed;
+    private boolean boosting;
+    private Rect hitBox;
+    private int shipShield = 2;
+
+    public PlayerShip(Context context, int maxX, int maxY) {
+        x = 50;
+        y = 50;
+        speed = 1;
+        boosting = false;
+        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.ship);
+        this.maxY = maxY - bitmap.getHeight();
+        this.minY = 0;
+        hitBox = new Rect(x, y, bitmap.getWidth(), bitmap.getHeight());
+    }
+
+    public Rect getHitBox() {
+        return hitBox;
+    }
+
+    public void startBoost() {
+        boosting = true;
+    }
+
+    public void stopBoost() {
+        boosting = false;
+    }
+
+    public int getShield() { return shipShield; }
+
+    public void decreaseShield() {
+        shipShield--;
+    }
+
+    public void update() {
+        // Are we boosting?
+        if (boosting) {
+            // Speed up
+            speed += 2;
+        } else {
+            // Slow down
+            speed -= 3;
+        }
+        // Constrain top speed
+        if (speed > MAX_SPEED) {
+            speed = MAX_SPEED;
+        }
+        // Never stop completely
+        if (speed < MIN_SPEED) {
+            speed = MIN_SPEED;
+        }
+        // move the ship up or down
+        y -= speed + GRAVITIY;
+        // But don't let ship stray off screen
+        if (y < minY) {
+            y = minY;
+        }
+        if (y > maxY) {
+            y = maxY;
+        }
+        // Refresh hit box location
+        hitBox.left = x;
+        hitBox.top = y;
+        hitBox.right = x + bitmap.getWidth();
+        hitBox.bottom = y + bitmap.getHeight();
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ app/src/main/java/com/imonguer/monguerspace/TDView.java
@@ -0,0 +1,290 @@
+package com.imonguer.monguerspace;
+
+/**
+ * Created by Usuario on 23/07/2016.
+ */
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.media.MediaPlayer;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class TDView extends SurfaceView implements
+        Runnable{
+
+    volatile boolean playing;
+    Thread gameThread = null;
+    private PlayerShip player;
+    private Paint paint;
+    private Paint invulnerabilityPaint;
+    private Canvas canvas;
+    private SurfaceHolder ourHolder;
+    private List<EnemyShip> enemies = new ArrayList<>();
+    private List<SpaceDust> dusts = new ArrayList<SpaceDust>();
+    private int points;
+    private long timeTaken;
+
+    private long highestPoints;
+    private int screenX;
+    private int screenY;
+    private Context context;
+    private boolean gameEnded = false;
+    MediaPlayer mediaPlayer = null;
+    private boolean invulnerability = false;
+    private Date invulnerabilityTimer;
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editor;
+
+    public TDView(Context context, int maxX, int maxY) {
+        super(context);
+        this.context = context;
+        ourHolder = getHolder();
+        paint = new Paint();
+        invulnerabilityPaint = new Paint();
+        screenX = maxX;
+        screenY = maxY;
+        mediaPlayer = MediaPlayer.create(context, R.raw.flappenny);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.start();
+            }
+        });
+
+        prefs = context.getSharedPreferences("HiScores",
+                Context.MODE_PRIVATE);
+        editor = prefs.edit();
+        highestPoints = prefs.getLong("highestPoints", 0);
+        startGame();
+    }
+
+    public void startGame() {
+
+        points = 0;
+        player = new PlayerShip(context, screenX, screenY);
+        enemies.clear();
+        dusts.clear();
+        for(int i = 0; i < 4; i++) {
+            enemies.add(new EnemyShip(context, screenX, screenY));
+        }
+        for(int i = 0; i < 40; i++) {
+            dusts.add(new SpaceDust(screenX, screenY));
+        }
+        // Reset time and distance
+        timeTaken = 0;
+        gameEnded = false;
+    }
+
+    @Override
+    public void run() {
+        while (playing) {
+            update();
+            draw();
+            control();
+        }
+
+    }
+
+    public void pause() {
+        playing = false;
+        mediaPlayer.pause();
+        try {
+            gameThread.join();
+        } catch (InterruptedException e) {
+
+        }
+
+    }
+
+    public void resume() {
+        playing = true;
+        mediaPlayer.start();
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    private void update() {
+        boolean addEnemy = false;
+
+        if (player.getShield() < 0) {
+            endGame();
+        }
+
+        if(!gameEnded) {
+            List<EnemyShip> enemyCopies = new ArrayList<EnemyShip>();
+            enemyCopies.addAll(enemies);
+            if(!invulnerability) {
+                for (final EnemyShip enemy : enemyCopies) {
+                    if (Rect.intersects(player.getHitBox(), enemy.getHitBox())) {
+                        enemy.setX(-200);
+                        player.decreaseShield();
+                        invulnerability = true;
+                        invulnerabilityTimer = new Date();
+                    }
+                }
+            }
+
+            if(invulnerability && null != invulnerabilityTimer && (invulnerabilityTimer.getTime() + 3000) <= new Date().getTime()){
+                invulnerability = false;
+            }
+
+
+            player.update();
+            for (final EnemyShip enemy : enemyCopies) {
+                enemy.update(player.getSpeed());
+                if (enemy.isSurpassed()) {
+                    points++;
+                    if (points % 100 == 0) {
+                        addEnemy = true;
+                    }
+                    enemy.setSurpassed(false);
+                }
+            }
+            if (addEnemy) {
+                enemies.add(new EnemyShip(context, screenX, screenY));
+            }
+
+            for (final SpaceDust dust : dusts) {
+                dust.update(player.getSpeed());
+            }
+        }
+
+    }
+
+    private void draw() {
+        if (ourHolder.getSurface().isValid()) {
+            //First we lock the area of memory we will be drawing to
+            canvas = ourHolder.lockCanvas();
+            // Rub out the last frame
+            canvas.drawColor(Color.argb(255, 0, 0, 0));
+            List<EnemyShip> enemyCopies = new ArrayList<EnemyShip>();
+            enemyCopies.addAll(enemies);
+
+            for (final EnemyShip enemy: enemyCopies) {
+                canvas.drawBitmap(
+                        enemy.getBitmap(),
+                        enemy.getX(),
+                        enemy.getY(),
+                        paint);
+            }
+
+            paint.setColor(Color.argb(255, 255, 255, 255));
+
+            for (final SpaceDust dust : dusts) {
+                canvas.drawPoint(dust.getX(), dust.getY(), paint);
+            }
+
+            invulnerabilityPaint.setStyle(Paint.Style.STROKE );
+            invulnerabilityPaint.setTextSize(80);
+
+            if(invulnerability) {
+
+                invulnerabilityPaint.setColor(Color.WHITE);
+                canvas.drawText("Invulnerable 3''!!" ,screenX/2 - screenX/4 + screenX/10, screenY/2 , invulnerabilityPaint);
+                canvas.drawCircle(player.getX() + player.getBitmap().getWidth() / 2,
+                        player.getY() + player.getBitmap().getHeight() / 2, 100, invulnerabilityPaint);
+            }else{
+                invulnerabilityPaint.setColor(Color.BLACK);
+                canvas.drawCircle(player.getX() + player.getBitmap().getWidth() / 2,
+                        player.getY() + player.getBitmap().getHeight() / 2, 100, invulnerabilityPaint);
+
+            }
+
+
+
+            // Draw the player
+            canvas.drawBitmap(
+                    player.getBitmap(),
+                    player.getX(),
+                    player.getY(),
+                    paint);
+
+            paint.setColor(Color.CYAN);
+            paint.setTextSize(60);
+            canvas.drawText(String.valueOf(points), 100, 100, paint);
+            if(!gameEnded) {
+                // Draw the hud
+                paint.setTextAlign(Paint.Align.LEFT);
+                paint.setColor(Color.argb(255, 255, 255, 255));
+                paint.setTextSize(25);
+                canvas.drawText("Fastest:"+ highestPoints + "s", 10, 20, paint);
+                canvas.drawText("Time:" + timeTaken + "s", screenX / 2, 20,
+                        paint);
+                canvas.drawText("Shield:" +
+                        player.getShield(), 10, screenY - 20, paint);
+
+                canvas.drawText("Speed:" +
+                        player.getSpeed() * 60 +
+                        " MPS", (screenX /3 ) * 2, screenY - 20, paint);
+            }
+            else {
+                // Show pause screen
+                paint.setTextSize(80);
+                paint.setTextAlign(Paint.Align.CENTER);
+                canvas.drawText("Game Over", screenX/2, 100, paint);
+                paint.setTextSize(25);
+                canvas.drawText("Highest:"+
+                        highestPoints + "s", screenX/2, 160, paint);
+
+                canvas.drawText("Time:" + timeTaken +
+                        "s", screenX / 2, 200, paint);
+
+                paint.setTextSize(80);
+                canvas.drawText("Tap to replay!", screenX/2, 350, paint);
+            }
+            // Unlock and draw the scene
+            ourHolder.unlockCanvasAndPost(canvas);
+
+        }
+    }
+
+    // SurfaceView allows us to handle the onTouchEvent
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        // There are many different events in MotionEvent
+        // We care about just 2 - for now.
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+
+            // Has the player lifted their finger up?
+            case MotionEvent.ACTION_UP:
+                player.stopBoost();
+                break;
+            // Has the player touched the screen?
+            case MotionEvent.ACTION_DOWN:
+                player.startBoost();
+                if(gameEnded) {
+                    startGame();
+                }
+                break;
+        }
+        return true;
+    }
+
+    private void control() {
+        try {
+            Thread.sleep(17);
+        } catch (InterruptedException e) {
+            //Vacío por diseño.
+        }
+    }
+
+    private void endGame() {
+        gameEnded = true;
+        invulnerability = false;
+        if(highestPoints < points){
+            highestPoints = points;
+            editor.putLong("highestPoints", highestPoints);
+            editor.commit();
+        }
+    }
+}
