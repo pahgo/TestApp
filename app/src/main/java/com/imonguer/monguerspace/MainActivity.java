5668dd54282e5825cd0041b7f854961589573ea7
spenalva <penalvapeni@gmail.com>
2016-08-04 20:29:08 +0200

    V0.2.3 - Added Google Play Services Utils Library

--- app/src/main/java/com/imonguer/monguerspace/MainActivity.java
+++ app/src/main/java/com/imonguer/monguerspace/MainActivity.java
@@ -1,6 +1,5 @@
 package com.imonguer.monguerspace;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -8,7 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+import com.google.example.games.basegameutils.BaseGameActivity;
+
+public class MainActivity extends BaseGameActivity implements View.OnClickListener {
 
     SharedPreferences prefs;
     SharedPreferences.Editor editor;
@@ -58,5 +59,15 @@ public class MainActivity extends Activity implements View.OnClickListener {
         long fastestTime = prefs.getLong("highestPoints", 0);
         // Put the high score in our TextView
         textFastestTime.setText("Puntuación máxima: " + fastestTime);
+    }
+
+    @Override
+    public void onSignInFailed() {
+        System.out.println("ERROR");
+    }
+
+    @Override
+    public void onSignInSucceeded() {
+        System.out.println("BIEEEEEEN");
     }
 }
